47c0773eed4d14ccc207c38fe81255f208772ed1
Joaquin Grez <joaquingrez@gmail.com>
2017-07-14 19:52:55 -0400

    Premium set to false

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,12 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :price, presence: true
+
+  before_save :set_premium 
+
+
+  def set_premium 
+  	self.premium ||= false
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170714232444) do
+ActiveRecord::Schema.define(version: 20170714233630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20170714232444) do
     t.integer  "price"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.boolean  "premium"
     t.index ["category_id"], name: "index_products_on_category_id", using: :btree
   end
 
