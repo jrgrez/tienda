a47af1072df03ec5b1a17a799131a8ad62af8212
Joaquin Grez <joaquingrez@gmail.com>
2017-07-14 20:29:20 -0400

    Upcase category name

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+Category.destroy_all
+
+
+c1 = Category.create(name: "Tecnologia")
+c2 = Category.create(name: "Alimentos")
+c3 = Category.create(name: "Muebles")
+
+Product.create(name: "Mouse", price: "100" ,category: c1)
+Product.create(name: "Sofa", price: "200",category: c3)
+
+c2.products.build(name: "Arroz", price: "10").save
+c1.products.build(name: "Cable", price: "20").save
+
+Product.create(name: "Sillon",price: "500",category: c3)
+c2.products.build(name: "Sal", price: "3").save
+
+a = Product.last
+a.premium = true
+a.save
+
+c = Category.all
+
+c.each do |x|
+	x.name.upcase!
+	x.save!
+end
