5c7ee55d1a473b146fd9d61dee148732dad3460e
Joaquin Grez <joaquingrez@gmail.com>
2017-07-14 21:27:51 -0400

    Backend avanzado

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,26 @@
 class Product < ApplicationRecord
   belongs_to :category
-  validates :price, presence: true
 
+  validates :price, presence: true
+  validates_numericality_of :discount, greater_than_or_equal_to: 0, less_than_or_equal_to: 100
+  #validates_numericality_of :discount
+  
+  after_destroy :erase_last
   before_save :set_premium 
 
+  scope :premium, -> {where(premium: true)}
+  scope :last_n, ->(numero) {Product.order("id DESC").limit(numero)}
+
+  def precio_final
+  	self.price - self.discount
+  end
 
   def set_premium 
   	self.premium ||= false
+  end
+
+  def erase_last
+ 	self.category.destroy if self.category.products.count == 0
   end
 
 end
